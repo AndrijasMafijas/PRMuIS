7afbe70b4c3be8e70d43ec4a6d24d49f0a5d7ab2
Andrija Stanisic <andrijanstanisic321@gmail.com>
2025-01-12 18:39:14 +0100

    Kreiranje prostog servera/klijenta

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,36 @@ namespace Client
     {
         static void Main(string[] args)
         {
+       
+                    // Kreiranje utičnice za slanje podataka
+                    Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                    ProtocolType.Udp);
+
+
+                    // Podešavanje adrese primaoca
+                    IPEndPoint recvEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.7"), 50000);
+
+
+                    // Poruka za slanje
+                    string message = "Veza je uspostavljena. Port je 50000 i udp je protokol.";
+                    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+                    // Slanje datagrama
+                    try
+                    {
+                        int bytesSent = sendSocket.SendTo(messageBytes, 0, messageBytes.Length,
+                       SocketFlags.None, recvEndPoint);
+                        Console.WriteLine("Sent {0} bytes to {1}", bytesSent, recvEndPoint);
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine("sendto failed with error: {0}", ex.Message);
+                    }
+                    // Zatvaranje utičnice nakon slanja
+                    sendSocket.Close();
+                
+                Console.ReadKey();
+            
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,58 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Odaberite koji cete protokol koristiti:");
+            Console.WriteLine("Ukoliko zelite TCP unesite 1 , a ukoliko zelite UDP unesite 2:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            bool dobar = false;
+            while (!dobar)
+            {
+
+                if (a == 1)
+                {
+                    dobar = true;
+                    // Kreiranje utičnice za prijem datagrama
+                    Socket recvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                    ProtocolType.Udp);
+
+
+                    // Povezivanje utičnice sa bilo kojom adresom na lokalnom računaru i portom 27015
+                    IPEndPoint recvEndPoint = new IPEndPoint(IPAddress.Any, 50000);
+                    recvSocket.Bind(recvEndPoint);
+
+
+                    // Bafer za prijem podataka
+                    byte[] recvBuf = new byte[1024];
+                    EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                    try
+                    {
+                        // Prijem datagrama
+                        int bytesReceived = recvSocket.ReceiveFrom(recvBuf, ref senderEndPoint);
+
+                        // Konverzija primljenih bajtova u string
+                        string receivedMessage = Encoding.UTF8.GetString(recvBuf, 0, bytesReceived);
+                        Console.WriteLine("Received {0} bytes from {1}: {2}", bytesReceived,
+                       senderEndPoint, receivedMessage);
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine("recvfrom failed with error: {0}", ex.Message);
+                    }
+                    // Zatvaranje utičnice nakon prijema
+                    recvSocket.Close();
+                    Console.ReadKey();
+                }
+                else if (a == 2)
+                {
+                    dobar = true;
+                }
+                else
+                {
+                    Console.WriteLine("Morate izabrati 1 ili 2!");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
