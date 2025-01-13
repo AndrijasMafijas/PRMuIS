5fb7c0aba5cf0ce8e68735786e4e47fb840a913e
Andrija Stanisic <andrijanstanisic321@gmail.com>
2025-01-13 20:58:03 +0100

    Omogucen izbor klijentu koji ce protokol da se koristi kao i dalja komunikacija tim protokolom

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,36 +12,171 @@ namespace Client
     {
         static void Main(string[] args)
         {
-       
+
+            
+            byte[] buffer = new byte[1024];
+            bool dobar = false;
+            while (!dobar)
+            {
+                Console.WriteLine("Odaberite koji cete protokol koristiti:");
+                Console.WriteLine("Ukoliko zelite TCP unesite 1 , a ukoliko zelite UDP unesite 2:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Odaberite koje cete sifrfovanje koristiti:");
+                Console.WriteLine("Ukoliko zelite 3DES unesite 1 , a ukoliko zelite RSA unesite 2:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                if ((a == 1 || a == 2) && (b == 1 || b == 2))
+                {
                     // Kreiranje utičnice za slanje podataka
-                    Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                    Socket odabirSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
                     ProtocolType.Udp);
-
-
                     // Podešavanje adrese primaoca
-                    IPEndPoint recvEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.7"), 50000);
-
-
+                    IPEndPoint odabirEP = new IPEndPoint(IPAddress.Parse("192.168.1.7"), 27015);
                     // Poruka za slanje
-                    string message = "Veza je uspostavljena. Port je 50000 i udp je protokol.";
-                    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                    string odabir = a.ToString() + " " + b.ToString();
+                    byte[] brojBajtova = Encoding.UTF8.GetBytes(odabir);
 
-                    // Slanje datagrama
+                    // Slanje odabira
                     try
                     {
-                        int bytesSent = sendSocket.SendTo(messageBytes, 0, messageBytes.Length,
-                       SocketFlags.None, recvEndPoint);
-                        Console.WriteLine("Sent {0} bytes to {1}", bytesSent, recvEndPoint);
+                        int bytesSent = odabirSocket.SendTo(brojBajtova, 0, brojBajtova.Length,
+                       SocketFlags.None, odabirEP);
+                        Console.WriteLine("Sent {0} bytes to {1}", bytesSent, odabirEP);
                     }
                     catch (SocketException ex)
                     {
                         Console.WriteLine("sendto failed with error: {0}", ex.Message);
                     }
                     // Zatvaranje utičnice nakon slanja
+                    odabirSocket.Close();
+                }
+
+                if (a == 1)
+                {
+                    dobar = true;
+                    #region PovezivanjeTCP
+                    Console.WriteLine("Izabrali ste tcp.");
+                    Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse("192.168.1.7"), 50001);
+                    
+
+                    Console.WriteLine("Klijent je spreman za povezivanje sa serverom, kliknite enter");
+                    Console.ReadKey();
+                    clientSocket.Connect(serverEP);
+                    Console.WriteLine("Klijent je uspesno povezan sa serverom! Protokol je TCP, a port 50001.");
+                    #endregion
+
+                    #region KomunikacijaTCP
+                    while (true)
+                    {
+                        Console.WriteLine("Unesite poruku za server:");
+                        try
+                        {
+                            string poruka = Console.ReadLine();
+                            int brBajta = clientSocket.Send(Encoding.UTF8.GetBytes(poruka));
+
+                            if (poruka == "kraj")
+                                break;
+
+                            Console.WriteLine("Poslato {0} bajtova na {1}", brBajta, serverEP);
+
+                            brBajta = clientSocket.Receive(buffer);
+
+                            if (brBajta == 0)
+                            {
+                                Console.WriteLine("Server je zavrsio sa radom");
+                                break;
+                            }
+
+                            string odgovor = Encoding.UTF8.GetString(buffer,0,brBajta);
+
+                            Console.WriteLine(odgovor);
+                            if (odgovor == "kraj")
+                                break;
+
+                        }
+                        catch (SocketException ex)
+                        {
+                            Console.WriteLine($"Doslo je do greske tokom slanja:\n{ex}");
+                            break;
+                        }
+
+                    }
+                    #endregion
+
+                    #region ZatvaranjeTCP
+                    Console.WriteLine("Klijent zavrsava sa radom");
+                    clientSocket.Close();
+                    Console.ReadKey();
+                    #endregion
+                }
+                else if (a == 2)
+                {
+                     dobar = true;
+                    Console.WriteLine("Izabrali ste udp.");
+                     // Kreiranje utičnice za slanje podataka
+                     Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                     ProtocolType.Udp);
+
+
+                    // Podešavanje adrese primaoca
+                    IPEndPoint recvEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.7"), 50000);
+                    EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                    while (true)
+                    {
+                        // Poruka za slanje
+                        Console.WriteLine("Unesite poruku za server:");
+                        try
+                        {
+                            string message = Console.ReadLine();
+                            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+                            
+                            // Slanje datagrama
+
+                            int bytes = sendSocket.SendTo(messageBytes, 0, messageBytes.Length,
+                            SocketFlags.None, recvEndPoint);
+
+                            if (message == "kraj")
+                                break;
+                            Console.WriteLine("Poslato {0} bajtova na {1}", bytes, recvEndPoint);
+
+
+                            bytes = sendSocket.ReceiveFrom(buffer, ref senderEndPoint);
+
+                            if (bytes == 0)
+                            {
+                                Console.WriteLine("Server je zavrsio sa radom");
+                                break;
+                            }
+
+                            Console.WriteLine("Server:");
+                            string odgovor = Encoding.UTF8.GetString(buffer , 0,bytes);
+                            Console.WriteLine(odgovor);
+                            if (odgovor == "kraj")
+                                break;
+
+                        }
+                        catch (SocketException ex)
+                        {
+                            Console.WriteLine($"Doslo je do greske tokom slanja:\n{ex}");
+                            break;
+                        }
+                        
+                    }
+
+                    // Zatvaranje utičnice nakon slanja
+                    Console.WriteLine("Klijent zavrsava sa radom");
                     sendSocket.Close();
-                
-                Console.ReadKey();
-            
+                    Console.ReadKey();
+
+                }
+                else
+                {
+                    Console.WriteLine("Morate uneti 1 ili 2");
+                }
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -12,58 +12,183 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Odaberite koji cete protokol koristiti:");
-            Console.WriteLine("Ukoliko zelite TCP unesite 1 , a ukoliko zelite UDP unesite 2:");
-            int a = Convert.ToInt32(Console.ReadLine());
-            bool dobar = false;
-            while (!dobar)
-            {
+            byte[] buffer = new byte[1024];
+            int a = 0,b = 0;
+            // Kreiranje utičnice za prijem datagrama
+            Socket recvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+            ProtocolType.Udp);
 
+
+            // Povezivanje utičnice sa bilo kojom adresom na lokalnom računaru i portom 50000
+            IPEndPoint recvEndPoint = new IPEndPoint(IPAddress.Any, 27015);
+            recvSocket.Bind(recvEndPoint);
+
+
+            // Bafer za prijem podataka
+            byte[] recvBuf = new byte[1024];
+            EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                // Prijem datagrama
+                int bytesReceived = recvSocket.ReceiveFrom(recvBuf, ref senderEndPoint);
+
+                // Konverzija primljenih bajtova u string
+                string receivedMessage = Encoding.UTF8.GetString(recvBuf, 0, bytesReceived);
+                //Console.WriteLine(receivedMessage);
+                a = Convert.ToInt32(receivedMessage[0]);
+                b = Convert.ToInt32(receivedMessage[2]);
                 if (a == 1)
                 {
-                    dobar = true;
-                    // Kreiranje utičnice za prijem datagrama
-                    Socket recvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
-                    ProtocolType.Udp);
-
-
-                    // Povezivanje utičnice sa bilo kojom adresom na lokalnom računaru i portom 27015
-                    IPEndPoint recvEndPoint = new IPEndPoint(IPAddress.Any, 50000);
-                    recvSocket.Bind(recvEndPoint);
-
-
-                    // Bafer za prijem podataka
-                    byte[] recvBuf = new byte[1024];
-                    EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
-
-                    try
-                    {
-                        // Prijem datagrama
-                        int bytesReceived = recvSocket.ReceiveFrom(recvBuf, ref senderEndPoint);
-
-                        // Konverzija primljenih bajtova u string
-                        string receivedMessage = Encoding.UTF8.GetString(recvBuf, 0, bytesReceived);
-                        Console.WriteLine("Received {0} bytes from {1}: {2}", bytesReceived,
-                       senderEndPoint, receivedMessage);
-                    }
-                    catch (SocketException ex)
-                    {
-                        Console.WriteLine("recvfrom failed with error: {0}", ex.Message);
-                    }
-                    // Zatvaranje utičnice nakon prijema
-                    recvSocket.Close();
-                    Console.ReadKey();
-                }
-                else if (a == 2)
-                {
-                    dobar = true;
+                    Console.WriteLine("Client je odlucio da se koristi TCP");
                 }
                 else
                 {
-                    Console.WriteLine("Morate izabrati 1 ili 2!");
+                    Console.WriteLine("Client je odlucio da se koristi UDP");
                 }
+                if (b == 1)
+                {
+                    Console.WriteLine("Client je odlucio da se koristi 3DES");
+                }
+                else
+                {
+                    Console.WriteLine("Client je odlucio da se koristi RSA");
+                }
+                Console.WriteLine("Received {0} bytes from {1}: {2}", bytesReceived,
+               senderEndPoint, receivedMessage);
             }
-            Console.ReadKey();
+            catch (SocketException ex)
+            {
+                Console.WriteLine("recvfrom failed with error: {0}", ex.Message);
+            }
+            // Zatvaranje utičnice nakon prijema
+            recvSocket.Close();
+
+
+            if (a == 1)
+            {
+                Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 50001);
+
+                serverSocket.Bind(serverEP);
+
+                serverSocket.Listen(5);
+
+
+                Console.WriteLine($"Server je stavljen u stanje osluskivanja i ocekuje komunikaciju na {serverEP}");
+
+                Socket acceptedSocket = serverSocket.Accept();
+
+                IPEndPoint clientEP = acceptedSocket.RemoteEndPoint as IPEndPoint;
+                Console.WriteLine($"Povezao se novi klijent! Njegova adresa je {clientEP}");
+
+
+
+                while (true)
+                {
+                    try
+                    {
+                        int brBajta = acceptedSocket.Receive(buffer);
+                        if (brBajta == 0)
+                        {
+                            Console.WriteLine("Klijent je zavrsio sa radom");
+                            break;
+                        }
+                        string poruka = Encoding.UTF8.GetString(buffer,0,brBajta);
+                        Console.WriteLine(poruka);
+
+
+                        if (poruka == "kraj")
+                            break;
+
+
+                        Console.WriteLine("Unesite poruku");
+                        string odgovor = Console.ReadLine();
+
+                        brBajta = acceptedSocket.Send(Encoding.UTF8.GetBytes(odgovor));
+                        if (odgovor == "kraj")
+                            break;
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine($"Doslo je do greske {ex}");
+                        break;
+                    }
+
+                }
+
+                Console.WriteLine("Server zavrsava sa radom");
+                Console.ReadKey();
+                acceptedSocket.Close();
+                serverSocket.Close();
+
+            }
+            else
+            {
+                // Kreiranje utičnice za slanje podataka
+                
+                Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                ProtocolType.Udp);
+
+
+                // Podešavanje adrese primaoca
+                recvEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.7"), 50000);
+                senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                sendSocket.Bind(recvEndPoint);
+
+                Console.WriteLine("SenderEP: {0} recvEP: {1}", senderEndPoint, recvEndPoint);
+
+                while (true)
+                {
+                    
+                    try
+                    {
+                        int bytes = sendSocket.ReceiveFrom(buffer, ref senderEndPoint);
+
+                        if (bytes == 0)
+                        {
+                            Console.WriteLine("Klijent je zavrsio sa radom");
+                            break;
+                        }
+
+                        Console.WriteLine("Klijent:");
+                        string odgovor = Encoding.UTF8.GetString(buffer,0,bytes);
+                        Console.WriteLine(odgovor);
+                        if (odgovor == "kraj")
+                            break;
+
+                        // Poruka za slanje
+                        Console.WriteLine("Unesite poruku za klijenta:");
+                        string message = Console.ReadLine();
+                        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+
+                        // Slanje datagrama
+
+                        bytes = sendSocket.SendTo(messageBytes, 0, messageBytes.Length,
+                        SocketFlags.None, senderEndPoint);
+
+                        if (message == "kraj")
+                            break;
+                        Console.WriteLine("Poslato {0} bajtova na {1}", bytes, senderEndPoint);
+
+
+                        
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine($"Doslo je do greske tokom slanja:\n{ex}");
+                        break;
+                    }
+                    
+                }
+                // Zatvaranje utičnice nakon slanja
+                Console.WriteLine("Server zavrsava sa radom");
+                sendSocket.Close();
+                Console.ReadKey();
+            }
         }
     }
 }
